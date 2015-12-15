625d385032435ac9f9846b4d33f3de474251c291
Anindya Basu <akbasu@gmail.com>
2015-12-14 21:09:50 -0600

    Added route for Demo.

---
+++ WebApp/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApp.Controllers
+{
+    [EnableCors(origins: "http://run.plnkr.co", headers: "*", methods: "*")]
+    public class DemoController : ApiController
+    {
+        public async Task<IHttpActionResult> Get()
+        {
+            bool demoMode;
+            bool.TryParse(ConfigurationManager.AppSettings["DemoMode"], out demoMode);
+            
+            return await Task.Run(() => Ok(demoMode));
+        }
+    }
+}
